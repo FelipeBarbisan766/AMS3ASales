0939983a3316da687a41994c19fe77de9f44d97e
FelipeBarbisan766 <felipebarbisan766@gmail.com>
2024-04-02 22:44:45 -0300

    Get de Produtos

---
+++ AMS3ASales.API/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using AMS3ASales.API.Context;
+using AMS3ASales.API.Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AMS3ASales.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly AplicationDataContext _context;
+        public ProductController(AplicationDataContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public ActionResult<IEnumerable<Product>> Get()
+        {
+            return _context.Product.ToList();
+        }
+    }
+}
